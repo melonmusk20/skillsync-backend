a7a616f053bd97f21542b509509a4ca99ffab9ed
Purbasa Maharana <purbasamaharana@gmail.com>
2026-03-27 23:42:50 +0530

    Update SecurityConfig.java

--- src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
+++ src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .requestMatchers("/", "/error").permitAll()
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/customers/register", "/auth/login").permitAll()
+                .requestMatchers("/resume/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
